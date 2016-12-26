6c910688a715eebd2b0c3ffa8353dad96c280047
danisluis6 <vuongluis@hotmail.com>
2016-12-26 16:46:40 +0700

    ok

--- Toolbar/README.md
+++ Toolbar/README.md
@@ -6,4 +6,14 @@
 > Major: Programmer
 > Address: VietName
 
+- [AlertDialog](#alertdialog)
+  - [Version1](#version1)
+  - [RelationShip](#relationship)
 
+  => Popup: Definition Menu And Initialize Event on Any Widget to call Popup and show
+  => Toolbar: Definition Toolbar and Using include to set position toolbar in main layout.
+
+##Version1
+<p align="center">
+  <img src="https://github.com/danisluis6/Researching-Widget-Android/blob/version1/AlertDialog/version1_1.png">
+</p>
--- Toolbar/app/src/main/java/com/example/enclaveit/toolbar/MainActivity.java
+++ Toolbar/app/src/main/java/com/example/enclaveit/toolbar/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.enclaveit.toolbar;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar)this.findViewById(R.id.toolbar);
+        this.setSupportActionBar(toolbar);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        /** Copy menu of Popup Menu and Insert **/
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    /**
+     * Anymore read here
+     * https://guides.codepath.com/android/Using-the-App-ToolBar
+     */
 }
